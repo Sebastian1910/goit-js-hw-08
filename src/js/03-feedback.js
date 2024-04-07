7bc0c23a564e758f928f4ee3e21ce70ec7be4f17
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-04-07 19:58:20 +0200

    a

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('form.feedback-form');
 
+const formDataStr = localStorage.getItem9('feedback-form-state');
+if (formDataStr) {
+  const formData = JSON.parse(formDataStr);
+  form.elementSibling.email.value = formData.email;
+  form.elementSibling.message.value = formData.message;
+}
+
+form.addEventListener('imput', ev => {
+  const data = {
+    email: ev.target.elements.email.value,
+    message: ev.target.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+});
+
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+  console.log({
+    email: ev.target.elements.email.value,
+    message: ev.target.elements.message.value,
+  });
+  localStorage.clear();
+  form.clear();
+});
