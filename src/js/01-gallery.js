acd42b29edeeb6b7bc6aa48e34534aa7151b4631
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-04-07 11:44:09 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+const gallery = document.querySelector('.gallery');
+
+// Funkcja do dodawania elementów galerii
+function addGalleryItems() {
+  galleryItems.forEach(item => {
+    const li = document.createElement('li');
+    li.classList.add('gallery__item');
+
+    const a = document.createElement('a');
+    a.classList.add('gallery__link');
+    a.setAttribute('href', item.original);
+
+    const img = document.createElement('img');
+    img.classList.add('gallery__image');
+    img.setAttribute('src', item.preview);
+    img.setAttribute('alt', item.description);
+    img.setAttribute('data-source', item.original);
+
+    a.appendChild(img);
+    li.appendChild(a);
+    gallery.appendChild(li);
+  });
+}
+
+// Dodanie elementów galerii po załadowaniu zawartości DOM
+document.addEventListener('DOMContentLoaded', function () {
+  addGalleryItems();
+});
 
 console.log(galleryItems);
