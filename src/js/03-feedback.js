64a864bade2fb1baa4bac47349e5a00008f7b6e8
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-04-08 13:13:25 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,9 @@
+// Importowanie funkcji throttle z biblioteki Lodash
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('form.feedback-form');
 
+// Pobieranie danych z local storage
 const formDataStr = localStorage.getItem('feedback-form-state');
 if (formDataStr) {
   const formData = JSON.parse(formDataStr);
@@ -7,14 +11,21 @@ if (formDataStr) {
   form.elements.message.value = formData.message;
 }
 
-form.addEventListener('input', ev => {
+// Funkcja do zapisywania danych w local storage z opóźnieniem
+const saveFormDataThrottled = throttle(() => {
   const data = {
     email: form.elements.email.value,
     message: form.elements.message.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}, 500);
+
+// Nasłuchiwanie zdarzenia input i zapisywanie danych w local storage
+form.addEventListener('input', ev => {
+  saveFormDataThrottled();
 });
 
+// Nasłuchiwanie zdarzenia submit i czyszczenie local storage oraz pól formularza
 form.addEventListener('submit', ev => {
   ev.preventDefault();
   const data = {
